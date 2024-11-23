168c340ee2b969cd9c96404b85e077675ac6b9a2
Enric Viñas Mas <enric.vinas_mas@roche.com>
2024-11-23 18:28:37 +0100

    remove unused import

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -2,9 +2,7 @@ import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Link, useNavigate } from "@tanstack/react-router";
