3bd7f99c4a51b8166c22dbef708b2364dd0884c1
Alex Tello Vidal <46220983+alexindris@users.noreply.github.com>
2024-11-24 08:35:13 +0100

    Add Firebase integration with messaging support and update platform configurations

--- look4park/lib/main.dart
+++ look4park/lib/main.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 import 'src/graphql/graphql_config.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // Handle background messages
+  print("Handling a background message: ${message.messageId}");
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp();
+
+  // Initialize Hive for offline storage (GraphQL)
   await initHiveForFlutter();
 
+  // Set up the SettingsController
   final settingsController = SettingsController(SettingsService());
-
   await settingsController.loadSettings();
+
+  // Configure background message handling
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   // Initialize GraphQL Client
   final ValueNotifier<GraphQLClient> graphqlClient =
--- look4park/lib/src/map/map_item_list_view.dart
+++ look4park/lib/src/map/map_item_list_view.dart
@@ -2,15 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import '../settings/settings_view.dart';
 import 'map_item_details_view.dart';
 
-class SampleItemListView extends StatelessWidget {
+void _getDeviceToken(BuildContext context) async {
+  String? token = await FirebaseMessaging.instance.getToken();
+  print("Device Token: $token");
+
+  if (token != null) {
+    _registerDeviceToken(context, token);
+  }
+}
+
+void _registerDeviceToken(BuildContext context, String token) async {
+  const String mutation = """
+    mutation RegisterDevice(\$token: String!) {
+      registerDevice(pushToken: \$token) {
+        success
+      }
+    }
+  """;
+
+  final GraphQLClient client = GraphQLProvider.of(context).value;
+
+  try {
+    final QueryResult result = await client.mutate(
+      MutationOptions(
+        document: gql(mutation),
+        variables: {'token': token},
+      ),
+    );
+
+    if (result.hasException) {
+      print("Error saving device token: ${result.exception}");
+    } else {
+      final bool success = result.data?['registerDevice']['success'] ?? false;
+      if (success) {
+        print("Device token registered successfully!");
+      } else {
+        print("Failed to register device token.");
+      }
+    }
+  } catch (e) {
+    print("Error during token registration: $e");
+  }
+}
+
+class SampleItemListView extends StatefulWidget {
   const SampleItemListView({super.key});
 
   static const routeName = '/';
 
+  @override
+  State<SampleItemListView> createState() => _SampleItemListViewState();
+}
+
+class _SampleItemListViewState extends State<SampleItemListView> {
   final String query = """
     query GetParkings {
       allParkings {
@@ -24,6 +73,51 @@ class SampleItemListView extends StatelessWidget {
       }
     }
   """;
+
+  @override
+  void initState() {
+    super.initState();
+    _requestNotificationPermissions();
+    _configureFirebaseMessaging();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _getDeviceToken(context);
+    });
+  }
+
+  /// Request notification permissions
+  Future<void> _requestNotificationPermissions() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      print('User granted provisional permission');
+    } else {
+      print('User declined or has not accepted permission');
+    }
+  }
+
+  /// Configure Firebase Messaging
+  void _configureFirebaseMessaging() {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      if (message.notification != null) {
+        print(
+            'Foreground notification received: ${message.notification?.title}');
+        print('Message body: ${message.notification?.body}');
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('Notification clicked: ${message.notification?.title}');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
